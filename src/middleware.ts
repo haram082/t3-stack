6201637e7712c8b3339e118f9038e351e7885eb4
Haram Yoon <haramkim082@gmail.com>
2023-07-08 15:25:41 -0700

    add auth

---
+++ src/middleware.ts
@@ -0,0 +1,10 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({
+    beforeAuth: ()=> false
+});
+
+// Path: my-t3-app\src\config.ts
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
